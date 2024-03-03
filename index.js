86c4c615ad3a81889829569e2c001b9281e5dbb6
henryraccoon <henryraccoon@Ububtu22.04.myguest.virtualbox.org>
2024-03-03 22:44:08 +0000

    refactored and upgraded error handling

--- index.js
+++ index.js
@@ -9,161 +9,7 @@ const telegramBot = new TelegramBot(TELEGRAM_BOT_API_KEY, { polling: true });
 
 const userState = {};
 
-telegramBot.onText(/\/start/, (msg) => {
-  const chatId = msg.chat.id;
-  telegramBot.sendMessage(
-    msg.chat.id,
-    `${msg.chat.first_name}, welcome to Wayfarer bot🖐️. Please let me know your destination. (can be airport IATA code or city name)`
-  );
-  userState[chatId] = { step: 1, start: "" };
-});
-
-telegramBot.on("message", async (msg) => {
-  const chatId = msg.chat.id;
-  const messageText = msg.text;
-
-  if (messageText === "/start") return;
-
-  if (userState[chatId]) {
-    const { step } = userState[chatId];
-
-    switch (step) {
-      case 1:
-        userState[chatId].destination = await cityByIata(messageText);
-        userState[chatId].countryCode = await countryByCity(
-          userState[chatId].destination
-        );
-        const countryInfo = await infoAboutCountry(
-          userState[chatId].countryCode
-        );
-
-        if (!countryInfo) {
-          console.log("no country info");
-        } else {
-          userState[chatId].countryName = countryInfo.name.common;
-
-          userState[chatId].currencies = [];
-          Object.entries(countryInfo.currencies).forEach(
-            ([curCode, curObj]) => {
-              userState[chatId].currencies.push(curObj.name);
-            }
-          );
-
-          userState[chatId].languages = [];
-          Object.entries(countryInfo.languages).forEach(
-            ([lngCode, lngName]) => {
-              userState[chatId].languages.push(lngName);
-            }
-          );
-        }
-        const travelRisks = await riskLevelCountry(
-          userState[chatId].countryCode
-        );
-        if (travelRisks) {
-          const countryKeys = Object.keys(travelRisks);
-          const firstKey = countryKeys[0];
-          userState[chatId].advice = travelRisks[firstKey].advisory.message;
-        }
-
-        const emergencyServices = await emergencyServicesByCountry(
-          userState[chatId].countryCode
-        );
-        console.log(emergencyServices);
-        if (emergencyServices) {
-          userState[chatId].member112 = emergencyServices.member_112;
-          userState[chatId].genEmerNum = emergencyServices.dispatch.all[0];
-          userState[chatId].fireNum = emergencyServices.fire.all[0];
-          userState[chatId].ambulanceNum = emergencyServices.ambulance.all[0];
-          userState[chatId].policeNum = emergencyServices.police.all[0];
-        }
-
-        if (!userState[chatId].destination) {
-          break;
-          delete userState[chatId];
-        } else {
-          userState[chatId].step = 2;
-
-          console.log(userState[chatId]);
-
-          const emerSerMsg112 = `${userState[chatId].countryName} supports international recommendation of using number 112 as standartised emergency service number. In case of emergency call 112! Or if available you can use local numbers: General Emergency - ${userState[chatId].genEmerNum}, Police - ${userState[chatId].policeNum}, Ambulance - ${userState[chatId].ambulanceNum}, Fire Department - ${userState[chatId].fireNum} `;
-
-          const emerSerMsg = `In case of emergency call these numbers: General Emergency - ${userState[chatId].genEmerNum}, Police - ${userState[chatId].policeNum}, Ambulance - ${userState[chatId].ambulanceNum}, Fire Department - ${userState[chatId].fireNum} `;
-
-          telegramBot.sendMessage(
-            chatId,
-            `Great! You are taveling to ${userState[chatId].destination}, ${
-              userState[chatId].countryName
-            }! People in ${userState[chatId].countryName} speak ${
-              userState[chatId].languages.length === 1
-                ? userState[chatId].languages[0]
-                : userState[chatId].languages.join(" and ")
-            } and use ${
-              userState[chatId].currencies.length === 1
-                ? userState[chatId].currencies[0]
-                : userState[chatId].currencies.join(" and ")
-            }. According to travel-advisory.info: ${userState[chatId].advice} ${
-              userState[chatId].member112 ? emerSerMsg112 : emerSerMsg
-            } `
-          );
-
-          telegramBot.sendMessage(
-            chatId,
-            "If you would like more useful information please provide youre departure airport IATA code or city."
-          );
-          break;
-        }
-
-      case 2:
-        userState[chatId].start = await cityByIata(messageText);
-        if (!userState[chatId].start) {
-          break;
-          delete userState[chatId];
-        } else {
-          userState[chatId].step = 3;
-          telegramBot.sendMessage(
-            chatId,
-            `Wonderfull! You want to tavel from ${userState[chatId].start} to ${userState[chatId].destination}! ... info ...`
-          );
-          telegramBot.sendMessage(
-            chatId,
-            "If you would also want some more information about weather conditions or upcoming holidays please provide date of your trip."
-          );
-          break;
-        }
-
-      case 3:
-        userState[chatId].date = messageText;
-        if (!userState[chatId].date) {
-          break;
-          delete userState[chatId];
-        } else {
-          telegramBot.sendMessage(
-            chatId,
-            `Amaizing! So you want to tavel from ${userState[chatId].start} to ${userState[chatId].destination} on ${userState[chatId].date}! ... info ...`
-          );
-          const sendThanks = function () {
-            telegramBot.sendMessage(
-              chatId,
-              `Thank you for using Wayfarer bot. All your provided data will be deleted now. Bye.`
-            );
-          };
-          setTimeout(sendThanks, 2000);
-          console.log(userState[chatId]);
-          delete userState[chatId];
-          break;
-        }
-
-      default:
-        telegramBot.sendMessage(chatId, "Oops! Something went wrong.");
-        delete userState[chatId];
-    }
-  } else {
-    telegramBot.sendMessage(chatId, "Please start the converstion with /start");
-    delete userState[chatId];
-  }
-});
-
-// airport api
+// API CALLS
 
 const cityByIata = async (iata) => {
   try {
@@ -243,23 +89,300 @@ const emergencyServicesByCountry = async (country) => {
   }
 };
 
-// API city to country:
-// https://api.api-ninjas.com/v1/city?name=
+const getTimeZoneOffset = async (city) => {
+  try {
+    const res = await axios({
+      method: "GET",
+      url: `https://api.api-ninjas.com/v1/timezone?city=${city}`,
+      headers: {
+        "X-Api-Key": process.env.API_NINJAS_KEY,
+      },
+    });
 
-// API for country: (name, currency)
-// https://api.api-ninjas.com/v1/country?name=
+    if (res.status === 200) {
+      const tzName = res.data.timezone;
 
-// API for emergency services:
-// https://emergencynumberapi.com/api/country/{ iso code}
+      const resp = await axios({
+        method: "GET",
+        url: `https://worldtimeapi.org/api/timezone/${tzName}`,
+      });
 
-// api for safety index:
-// https://www.travel-advisory.info/api?countrycode=AU
+      if (resp.status === 200) {
+        return resp.data.utc_offset;
+      }
+    }
+  } catch (err) {
+    console.log(err.code, "could not find timezone info about a city");
+  }
+};
 
-// api for country info: language, currency, flag, carside, timezone etc
-// https://restcountries.com/v3.1/name/{name}
+const excRate = async function (query) {
+  try {
+    const res = await axios({
+      method: "GET",
+      url: `https://v6.exchangerate-api.com/v6/${process.env.EXCHANGE_RATE_API_KEY}/pair/${query}`,
+    });
 
-// api for holidays:
-// https://date.nager.at/api/v2/PublicHolidays/2024/US
+    if (res.status === 200) {
+      return res.data.conversion_rate;
+    }
+  } catch (err) {
+    console.log(err.code, "could not find exchange rate info");
+  }
+};
 
-// api for weather:
-// https://history.openweathermap.org/data/2.5/aggregated/year?lat=35&lon=139&appid={API key} need to register
+// HELPER FUNCTIONS
+
+const getExchangeRateQuery = function (startCur, destCur) {
+  if (startCur.length === 1 && destCur.length === 1) {
+    const query = [...startCur, ...destCur].join("/");
+
+    return query;
+  } else if (startCur.length > 1 || destCur.length > 1) {
+    const query = [startCur[0], destCur[0]].join("/");
+
+    return query;
+  }
+};
+
+function calculateTimeDifference(startTZ, destTZ) {
+  const startOffset = parseUtcOffset(startTZ);
+  const destOffset = parseUtcOffset(destTZ);
+
+  const timeDifferenceHours = (destOffset - startOffset) / 60;
+
+  return timeDifferenceHours;
+}
+
+function parseUtcOffset(utcOffset) {
+  const [sign, hours, minutes] = utcOffset
+    .match(/^([-+])(\d{2}):(\d{2})$/)
+    .slice(1);
+  const offsetMinutes =
+    (parseInt(hours, 10) * 60 + parseInt(minutes, 10)) *
+    (sign === "-" ? -1 : 1);
+  return offsetMinutes;
+}
+
+// TELEGRAM MESSAGE HANDLERS
+
+telegramBot.onText(/\/start/, (msg) => {
+  const chatId = msg.chat.id;
+  telegramBot.sendMessage(
+    msg.chat.id,
+    `${msg.chat.first_name}, welcome to Wayfarer bot🖐️. I will be happy to provide all basic but useful information about your upcoming trip. Please let me know your destination airport IATA code. (three letters that you can see on your ticket 🫡)`
+  );
+  userState[chatId] = { step: 1, start: "" };
+});
+
+telegramBot.on("message", async (msg) => {
+  const chatId = msg.chat.id;
+  const messageText = msg.text;
+
+  if (messageText === "/start") return;
+
+  if (userState[chatId]) {
+    const { step } = userState[chatId];
+
+    switch (step) {
+      case 1:
+        userState[chatId].destination = await cityByIata(messageText);
+        userState[chatId].countryCode = await countryByCity(
+          userState[chatId].destination
+        );
+
+        const countryInfo = await infoAboutCountry(
+          userState[chatId].countryCode
+        );
+
+        if (!userState[chatId].destination) {
+          console.log("something went wrong in step 1");
+          telegramBot.sendMessage(
+            chatId,
+            ` Something went wrong, because I couldn't find a city airport with that IATA. Maybe try again from the /start? If problem persists, maybe this airport is just isn't in my database. Sorry. I know I'm not perfect... But I will try to get better. 😔  `
+          );
+          break;
+          delete userState[chatId];
+        } else if (!countryInfo) {
+          telegramBot.sendMessage(
+            chatId,
+            ` Something went wrong, because I could find that you're going to ${userState[chatId].destination}, but I couldn't find information about a country... That happens when city names change, but aren't updated in all databases. Sorry. I know I'm not perfect... But I will try to get better. 😔 `
+          );
+          break;
+          delete userState[chatId];
+        } else {
+          userState[chatId].countryName = countryInfo.name.common;
+          userState[chatId].step = 2;
+
+          telegramBot.sendMessage(
+            chatId,
+            `Great! You are taveling to ${userState[chatId].destination}, ${userState[chatId].countryName}.`
+          );
+
+          userState[chatId].currencies = [];
+          userState[chatId].destCurCode = Object.keys(countryInfo.currencies);
+
+          Object.entries(countryInfo.currencies).forEach(
+            ([curCode, curObj]) => {
+              userState[chatId].currencies.push(curObj.name);
+            }
+          );
+
+          userState[chatId].languages = [];
+          Object.entries(countryInfo.languages).forEach(
+            ([lngCode, lngName]) => {
+              userState[chatId].languages.push(lngName);
+            }
+          );
+
+          const travelRisks = await riskLevelCountry(
+            userState[chatId].countryCode
+          );
+          if (travelRisks) {
+            const countryKeys = Object.keys(travelRisks);
+            const firstKey = countryKeys[0];
+            userState[chatId].advice = travelRisks[firstKey].advisory.message;
+          }
+
+          const emergencyServices = await emergencyServicesByCountry(
+            userState[chatId].countryCode
+          );
+
+          if (emergencyServices) {
+            userState[chatId].member112 = emergencyServices.member_112;
+            userState[chatId].genEmerNum = emergencyServices.dispatch.all[0];
+            userState[chatId].fireNum = emergencyServices.fire.all[0];
+            userState[chatId].ambulanceNum = emergencyServices.ambulance.all[0];
+            userState[chatId].policeNum = emergencyServices.police.all[0];
+          }
+
+          const emerSerMsg112 = `${userState[chatId].countryName} supports international recommendation of using number 112 as standartised emergency service number. In case of emergency call 112! Or if available you can use local numbers: General Emergency - ${userState[chatId].genEmerNum}, Police - ${userState[chatId].policeNum}, Ambulance - ${userState[chatId].ambulanceNum}, Fire Department - ${userState[chatId].fireNum} `;
+
+          const emerSerMsg = `In case of emergency call these numbers: General Emergency - ${userState[chatId].genEmerNum}, Police - ${userState[chatId].policeNum}, Ambulance - ${userState[chatId].ambulanceNum}, Fire Department - ${userState[chatId].fireNum} `;
+
+          telegramBot.sendMessage(
+            chatId,
+            `People in ${userState[chatId].countryName} speak ${
+              userState[chatId].languages.length === 1
+                ? userState[chatId].languages[0]
+                : userState[chatId].languages.join(" and ")
+            } and use ${
+              userState[chatId].currencies.length === 1
+                ? userState[chatId].currencies[0]
+                : userState[chatId].currencies.join(" and ")
+            }.`
+          );
+
+          telegramBot.sendMessage(
+            chatId,
+            `According to travel-advisory.info: ${userState[chatId].advice}`
+          );
+
+          telegramBot.sendMessage(
+            chatId,
+            `${userState[chatId].member112 ? emerSerMsg112 : emerSerMsg} `
+          );
+
+          const offeringMoreInformation = function () {
+            telegramBot.sendMessage(
+              chatId,
+              "If you would like more useful information please provide youre departure airport IATA."
+            );
+          };
+
+          setTimeout(offeringMoreInformation, 2000);
+
+          userState[chatId].destTZ = await getTimeZoneOffset(
+            userState[chatId].destination
+          );
+
+          break;
+        }
+
+      case 2:
+        userState[chatId].start = await cityByIata(messageText);
+        if (!userState[chatId].start) {
+          telegramBot.sendMessage(
+            chatId,
+            ` Something went wrong, because I couldn't find a city airport with that IATA. Maybe try again from the /start? If problem persists, maybe this airport is just isn't in my database. Sorry. I know I'm not perfect... But I will try to get better. 😔  `
+          );
+
+          break;
+          delete userState[chatId];
+        } else {
+          userState[chatId].step = 3;
+
+          userState[chatId].startCountryCode = await countryByCity(
+            userState[chatId].start
+          );
+
+          const startCountryInfo = await infoAboutCountry(
+            userState[chatId].startCountryCode
+          );
+
+          if (!startCountryInfo) {
+            telegramBot.sendMessage(
+              chatId,
+              ` Something went wrong, because I could find that your departure city is ${userState[chatId].start}, but I couldn't find information about a country... That happens when city names change, but aren't updated in all databases. And in this case, unfortunately, I won't be able to provide more information. Sorry. I know I'm not perfect... But I will try to get better. 😔 `
+            );
+            break;
+            delete userState[chatId];
+          }
+
+          userState[chatId].startCurCode = Object.keys(
+            startCountryInfo.currencies
+          );
+
+          userState[chatId].startTZ = await getTimeZoneOffset(
+            userState[chatId].start
+          );
+          const timeDifferenceHours = calculateTimeDifference(
+            userState[chatId].startTZ,
+            userState[chatId].destTZ
+          );
+
+          const exQuery = getExchangeRateQuery(
+            userState[chatId].startCurCode,
+            userState[chatId].destCurCode
+          );
+
+          const rate = await excRate(exQuery);
+
+          const noExchangeMsg = `Both your destination and departure countries use the same currency`;
+
+          const exchangeRateMsg = `The exchange rate ${userState[chatId].startCurCode} to ${userState[chatId].destCurCode} is ${rate}`;
+
+          const timeDifferMsg = `Time difference will be ${timeDifferenceHours} hours`;
+
+          telegramBot.sendMessage(
+            chatId,
+            `Wonderful! You are traveling from ${userState[chatId].start} to ${
+              userState[chatId].destination
+            }. ${timeDifferMsg}. ${
+              userState[chatId].startCurCode[0] ===
+              userState[chatId].destCurCode[0]
+                ? noExchangeMsg
+                : exchangeRateMsg
+            }.`
+          );
+
+          const sendThanks = function () {
+            telegramBot.sendMessage(
+              chatId,
+              `Thank you for using Wayfarer bot. All your provided data will be deleted now. Bye. 🤝 Have a nice trip!`
+            );
+          };
+          setTimeout(sendThanks, 2000);
+          delete userState[chatId];
+          break;
+        }
+
+      default:
+        telegramBot.sendMessage(chatId, "Oops! Something went wrong.");
+        delete userState[chatId];
+    }
+  } else {
+    telegramBot.sendMessage(chatId, "Please start the converstion with /start");
+    delete userState[chatId];
+  }
+});
